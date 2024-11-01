8a71d7da3f73d665bc8603c3043edfc7804ca640
criztian <criztiandev@gmail.com>
2024-11-01 18:54:54 +0800

    design: added Profile header

---
+++ template/basic/components/profile/profile-header/index.js
@@ -0,0 +1,33 @@
+import { Box, Avatar } from "@chakra-ui/react";
+import React from "react";
+
+const ProfileHeader = () => {
+  return (
+    <Box position="relative" marginBottom={8}>
+      <img
+        src="https://images.pexels.com/photos/28176437/pexels-photo-28176437/free-photo-of-scented-candles-decor.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        style={{
+          maxHeight: 200,
+          objectFit: "cover",
+          width: "100%",
+        }}
+      />
+      <Avatar
+        position="absolute"
+        left={0}
+        name="Dan Abrahmov"
+        src="https://bit.ly/dan-abramov"
+        width={24}
+        height={24}
+        border="2px solid white"
+        style={{
+          bottom: -42,
+          marginRight: 16,
+          marginLeft: 32,
+        }}
+      />
+    </Box>
+  );
+};
+
+export default ProfileHeader;
