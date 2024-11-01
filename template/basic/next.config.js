b54d840d783aec7267e9fc5178b56c5b677cd00c
criztian <criztiandev@gmail.com>
2024-11-01 19:04:53 +0800

    CI: added main process ID

--- template/basic/next.config.js
+++ template/basic/next.config.js
@@ -1,3 +1,8 @@
-const arnext = require("arnext/config")
-const nextConfig = { reactStrictMode: true }
-module.exports = arnext(nextConfig)
+const arnext = require("arnext/config");
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    MAIN_PROCESS_ID: "BAytmPejjgB0IOuuX7EmNhSv1mkoj5UOFUtt0HHOzr8",
+  },
+};
+module.exports = arnext(nextConfig);
