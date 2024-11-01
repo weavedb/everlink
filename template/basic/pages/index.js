f857e5cac72ea3775ca67ffe786f3c5a9cabc3d9
criztian <criztiandev@gmail.com>
2024-11-01 20:43:30 +0800

    feat: added responsiveness and dynamic data

--- template/basic/pages/index.js
+++ template/basic/pages/index.js
@@ -1,9 +1,20 @@
+"use client";
+
 import ProfileHeader from "@/components/profile/profile-header";
 import ProfileDetails from "@/components/profile/profile-details";
-import { Box, ChakraProvider, useToast, Divider } from "@chakra-ui/react";
+import {
+  Box,
+  ChakraProvider,
+  useToast,
+  Container,
+  Button,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
 import { ssr } from "arnext";
 import { useEffect, useState } from "react";
-import SocialCard from "@/components/card/social-card";
+import { useMediaQuery } from "@chakra-ui/react";
+import Head from "next/head";
 
 const getDate = async (date) => date ?? Date.now();
 const getFullUrl = async (url) => url ?? window.location.href;
@@ -25,10 +36,14 @@ const _resultData = {
 
 export default function Home({ _date = null, _fullUrl = null }) {
   const toast = useToast();
+  const [isLargeScreen] = useMediaQuery("(min-width: 700px)");
 
   const [credentials, setCredentials] = useState({
     UserName: "",
     Description: "",
+    SubDomain: "",
+    TransactionId: "",
+    Owner: "",
   });
 
   const [date, setDate] = useState(_date);
@@ -64,8 +79,7 @@ export default function Home({ _date = null, _fullUrl = null }) {
         // const jsonData = JSON.parse(_resultData);
         const jsonData = _resultData;
 
-        setCredentials((prev) => ({
-          ...prev,
+        setCredentials(() => ({
           ...jsonData,
         }));
 
@@ -77,7 +91,7 @@ export default function Home({ _date = null, _fullUrl = null }) {
       }
       _date ?? setDate(await getDate());
     })();
-  }, [_fullUrl, _date]);
+  }, []);
 
   const formatUrl = (url) => {
     if (!/^https?:\/\//i.test(url)) {
@@ -88,17 +102,79 @@ export default function Home({ _date = null, _fullUrl = null }) {
   return (
     <>
       <ChakraProvider>
-        <Box maxWidth="475" marginX="auto">
-          <ProfileHeader />
-          <ProfileDetails />
-          <Divider />
-          <Box display="flex" flexDir="column" gap={2} padding={4}>
-            <SocialCard />
-            <SocialCard />
-            <SocialCard />
-            <SocialCard />
-          </Box>
-        </Box>
+        <Head>
+          <title>Link Tree</title>
+        </Head>
+        <Container maxW="container.xl" p={4}>
+          <Grid
+            templateColumns={{
+              md: "35% 1fr",
+            }}
+            gap={4}
+          >
+            <GridItem>
+              <Box>
+                <ProfileHeader {...credentials} />
+                <ProfileDetails {...credentials} />
+              </Box>
+            </GridItem>
+            <GridItem>
+              <Box display="flex" flexDir="column" gap={3}>
+                <Button
+                  borderRadius="full"
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <span></span>
+                  <span>💡 Link</span>
+
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                  >
+                    <title>external_link_line</title>
+                    <g id="external_link_line" fill="none">
+                      <path d="M24 0v24H0V0zM12.593 23.258l-.011.002-.071.035-.02.004-.014-.004-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01-.017.428.005.02.01.013.104.074.015.004.012-.004.104-.074.012-.016.004-.017-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113-.013.002-.185.093-.01.01-.003.011.018.43.005.012.008.007.201.093c.012.004.023 0 .029-.008l.004-.014-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014-.034.614c0 .012.007.02.017.024l.015-.002.201-.093.01-.008.004-.011.017-.43-.003-.012-.01-.01z" />
+                      <path
+                        fill="#09244BFF"
+                        d="M11 6a1 1 0 1 1 0 2H5v11h11v-6a1 1 0 1 1 2 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2zm9-3a1 1 0 0 1 1 1v5a1 1 0 1 1-2 0V6.414l-8.293 8.293a1 1 0 0 1-1.414-1.414L17.586 5H15a1 1 0 1 1 0-2Z"
+                      />
+                    </g>
+                  </svg>
+                </Button>
+
+                <Button
+                  borderRadius="full"
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <span></span>
+                  <span>💡 Link</span>
+
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                  >
+                    <title>external_link_line</title>
+                    <g id="external_link_line" fill="none">
+                      <path d="M24 0v24H0V0zM12.593 23.258l-.011.002-.071.035-.02.004-.014-.004-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01-.017.428.005.02.01.013.104.074.015.004.012-.004.104-.074.012-.016.004-.017-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113-.013.002-.185.093-.01.01-.003.011.018.43.005.012.008.007.201.093c.012.004.023 0 .029-.008l.004-.014-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014-.034.614c0 .012.007.02.017.024l.015-.002.201-.093.01-.008.004-.011.017-.43-.003-.012-.01-.01z" />
+                      <path
+                        fill="#09244BFF"
+                        d="M11 6a1 1 0 1 1 0 2H5v11h11v-6a1 1 0 1 1 2 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2zm9-3a1 1 0 0 1 1 1v5a1 1 0 1 1-2 0V6.414l-8.293 8.293a1 1 0 0 1-1.414-1.414L17.586 5H15a1 1 0 1 1 0-2Z"
+                      />
+                    </g>
+                  </svg>
+                </Button>
+              </Box>
+            </GridItem>
+          </Grid>
+        </Container>
       </ChakraProvider>
     </>
   );
