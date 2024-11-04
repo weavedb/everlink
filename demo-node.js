59622f09993901af2201f4614d0639d2a622d144
drumfeet <drumfeet@gmail.com>
2024-11-04 15:12:03 +0100

    wip

--- demo-node.js
+++
@@ -1,9 +0,0 @@
-const { IO, ArweaveSigner } = require("@ar.io/sdk")
-const fs = require("fs")
-
-const jwk = JSON.parse(fs.readFileSync("arweave-keyfile.json"))
-
-const io = IO.init({
-  signer: new ArweaveSigner(jwk),
-})
-console.log(io)
