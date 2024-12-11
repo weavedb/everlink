f2af5b3c5ecac8dbcf2c367e7e237114c59c448f
drumfeet <drumfeet@gmail.com>
2024-12-11 18:06:05 +0100

    remove template/basic

--- template/basic/next.config.js
+++
@@ -1,8 +0,0 @@
-const arnext = require("arnext/config");
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    MAIN_PROCESS_ID: "BAytmPejjgB0IOuuX7EmNhSv1mkoj5UOFUtt0HHOzr8",
-  },
-};
-module.exports = arnext(nextConfig);
--- template/basic/pages/_app.js
+++
@@ -1,5 +0,0 @@
-import { ArNext } from "arnext"
-
-export default function App(props) {
-  return <ArNext {...props} />
-}
