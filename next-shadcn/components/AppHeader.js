75c865354be096dc78897c7c75b941c28510879c
drumfeet <drumfeet@gmail.com>
2025-02-05 17:08:11 +0100

    add disconnect in AppHeader

--- next-shadcn/components/AppHeader.js
+++ next-shadcn/components/AppHeader.js
@@ -10,10 +10,14 @@ import {
 } from "@/components/ui/dropdown-menu"
 import DiscordIcon from "./icons/DiscordIcon"
 import { useAppContext } from "@/context/AppContext"
+import { useToast } from "@/hooks/use-toast"
+import { LogOut } from "lucide-react"
 
 export function AppHeader() {
   const { connectWallet, disconnectWallet, isConnected, userAddress } =
     useAppContext()
+
+  const { toast } = useToast()
   return (
     <header className="border-b border-border/50 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex items-center px-6 py-3">
@@ -48,7 +52,15 @@ export function AppHeader() {
                 <div className="flex items-center">
                   <Wallet className="mr-2 h-4 w-4" />
                   {isConnected ? (
-                    <span className="truncate max-w-[150px]">
+                    <span
+                      className="truncate max-w-[150px]"
+                      onClick={() => {
+                        navigator.clipboard.writeText(userAddress)
+                        toast({
+                          description: "Wallet address copied to clipboard",
+                        })
+                      }}
+                    >
                       {userAddress?.slice(0, 5)}....{userAddress?.slice(-5)}
                     </span>
                   ) : (
@@ -56,6 +68,22 @@ export function AppHeader() {
                   )}
                 </div>
               </DropdownMenuItem>
+              {isConnected && (
+                <DropdownMenuItem
+                  onClick={async () => {
+                    disconnectWallet()
+                    toast({
+                      description: "Wallet disconnected",
+                      variant: "destructive",
+                    })
+                  }}
+                >
+                  <div className="flex items-center text-destructive">
+                    <LogOut className="mr-2 h-4 w-4" />
+                    Disconnect
+                  </div>
+                </DropdownMenuItem>
+              )}
               <DropdownMenuItem>
                 <a
                   href={`https://x.com/everlinkdotfun`}
